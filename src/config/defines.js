fc946363f4a984c76039d7113cab452074b7554d
Rosen Blagoev <rossencho@mail.ru>
2020-03-31 14:44:30 +0300

    2 commit

--- src/config/defines.js
+++
@@ -1,6 +0,0 @@
-export const MUSICMATCH_GET_TRACKS_URL =
-  "https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_track={%trackTitle%}&page_size=10&page=1&s_track_rating=desc&apikey=50662f725244b848179ab46330563aa4";
-export const MUSICMATCH_GET_LYRICS_URL =
-  "https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id={%id%}&apikey=50662f725244b848179ab46330563aa4";
-export const MUSICMATCH_GET_TRACK_URL =
-  "https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.get?track_id={%id%}&apikey=50662f725244b848179ab46330563aa4";
